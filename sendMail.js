134cbeddaccf68ae98e09a39e6a87531c7778d7c
Mohit Kumbhare <emkay@Mohits-MacBook-Air.local>
2023-05-09 15:24:48 +0530

    Final commit

--- sendMail.js
+++ sendMail.js
@@ -7,8 +7,8 @@ function sendMail(from, to, subject, text) {
       port: 587,
       secure: false, // true for 465, false for other ports
       auth: {
-          user: '***********',
-          pass: '***********'
+          user: '*****',
+          pass: '******'
       }
   });
 
